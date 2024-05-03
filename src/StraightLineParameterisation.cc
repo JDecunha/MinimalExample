f81d283fa4b9d48e5dd66e688d7c6553d32fb4b2
Joseph <joeydecunha@gmail.com>
2024-05-03 14:57:23 -0400

    Remove spacing between parameterised volumes.

--- src/StraightLineParameterisation.cc
+++ src/StraightLineParameterisation.cc
@@ -21,7 +21,7 @@ void StraightLineParameterisation::ComputeDimensions(G4Box& box, const G4int cop
 {
     box.SetXHalfLength(_halfx);
     box.SetYHalfLength(_halfy);
-    box.SetZHalfLength((_halfz/_zinc)*0.95); //Introduce the "fudge factor" to give a little spacing between boxes
+    box.SetZHalfLength(_halfz/_zinc); 
 }
 
 void StraightLineParameterisation::ComputeTransformation(const G4int copyNo, G4VPhysicalVolume* physical) const
